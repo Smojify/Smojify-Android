9e7aa3a6cc9992dba08b10673095c98affe4d50d
Mia <mia@Mia.local>
2023-05-26 17:17:32 +0200

    Add: Empty views

---
+++ app/src/main/java/com/smojify/smojify/UserPlaylistsActivity.java
@@ -0,0 +1,14 @@
+package com.smojify.smojify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPlaylistsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_playlists);
+    }
+}
