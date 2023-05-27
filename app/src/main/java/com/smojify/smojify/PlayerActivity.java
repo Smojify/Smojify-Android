21d2ed506035ddd8757dc9a5dce8a4b4318f7ad6
Mia <mia@Mia.local>
2023-05-27 13:05:56 +0200

    📦 Add Spotify SDK in gradle

--- app/src/main/java/com/smojify/smojify/PlayerActivity.java
+++ app/src/main/java/com/smojify/smojify/PlayerActivity.java
@@ -11,4 +11,20 @@ public class PlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    protected void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
 }
