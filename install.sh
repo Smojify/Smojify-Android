f29f5d7504e5144a01623b705b74b6335618199e
Théophile Delmas <contact@theophile.world>
2024-04-08 20:16:37 +0200

    Add Java check at install

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+REQUIRED_JAVA_VERSION="17.0.0"
+
+check_java_version() {
+echo "Checking Java Version"
+if ! which java &>- ;
+then
+	echo "Java not found, please install Java first"
+	exit 1
+else
+	JAVA_VERSION="$(java --version | head -n1 | cut -d' ' -f2)"
+	echo "Java Version: $JAVA_VERSION"
+	LOWEST_JAVA_VERSION="$(echo "$JAVA_VERSION\n$REQUIRED_JAVA_VERSION" | sort | head -n1)"
+	if [ "$LOWEST_JAVA_VERSION" != "$REQUIRED_JAVA_VERSION" ]
+	then
+		echo "Please update your java version to $REQUIRED_JAVA_VERSION or later"
+		exit 1
+	fi
+fi
+}
+
+
 echo "Smojify Installer"
+check_java_version
+
 echo "Downloading Spotify libs"
 mkdir -pv 'app/libs'
 wget 'https://github.com/spotify/android-sdk/releases/download/v0.7.2-appremote_v1.2.3-auth/spotify-app-remote-release-0.7.2.aar'
